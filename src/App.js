ef75e1488e63d3b69199e90a86cd16f803529a88
lordvalmir <lukas.kopacka@email.cz>
2019-07-22 09:56:33 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -97,7 +97,6 @@ class App extends Component {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
             .catch(console.log)
-
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
